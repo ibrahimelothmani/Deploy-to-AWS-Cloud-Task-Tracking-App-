f41fc911d2383390dcde051f52b852c01dacdbff
Ibrahim <ibrahimelothmani98@gmail.com>
2024-12-27 10:55:20 +0100

    create the TaskListController and his methods

---
+++ src/main/java/com/ibrahim/tasks/controllers/TaskListController.java
@@ -0,0 +1,33 @@
+package com.ibrahim.tasks.controllers;
+
+
+import com.ibrahim.tasks.dto.TaskListDto;
+import com.ibrahim.tasks.mapper.TaskListMapper;
+import com.ibrahim.tasks.services.TaskListService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/task-lists")
+public class TaskListController {
+
+    private final TaskListService taskListService;
+    private final TaskListMapper taskListMapper;
+
+    public TaskListController(TaskListService taskListService, TaskListMapper taskListMapper) {
+        this.taskListService = taskListService;
+        this.taskListMapper = taskListMapper;
+    }
+
+    @GetMapping
+    public List<TaskListDto> listTaskLists() {
+        return taskListService.listTaskLists()
+                .stream()
+                .map(taskListMapper::toDto)
+                .toList();
+    }
+}
