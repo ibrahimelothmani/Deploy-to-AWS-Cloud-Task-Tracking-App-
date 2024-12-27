60ab3caed61c03c5409f28b9f80fa5a1d5f56a98
Ibrahim <ibrahimelothmani98@gmail.com>
2024-12-27 10:35:02 +0100

    CREAT OUR REPOSITORY and the necessary methods

---
+++ src/main/java/com/ibrahim/tasks/repositories/TaskListRepository.java
@@ -0,0 +1,12 @@
+package com.ibrahim.tasks.repositories;
+
+import com.ibrahim.tasks.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+
+}
